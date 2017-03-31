c611309e2ffaaa04021fa80b9389c4926d95e327
Azraelos <andrewsugiarto93@gmail.com>
2017-03-31 16:28:48 +0800

    Lab 5 31/3/2017

---
+++ Lab56/Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/Andrew/workspace_c++/freeRTOS/Release/libfreeRTOS.a
+
+LIBS :=
+
